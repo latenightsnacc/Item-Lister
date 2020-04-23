320ded45fb1e70cac953635e4a26a23e3c24dd9d
latenightsnacc <egonubuchi97@gmail.com>
2020-04-23 09:16:56 -0400

    Traversed the DOM using parentNode, parentElement, childNode, children, nextSibiling, nextElementSibling, previousSibling, previousElementSibling, firstChild, lastChild, firstElementChild, lastElementChild, created a new div element using createElement, set a class using the className methd, set an id using the id method, set an attribute using the setAtrribute method. Created a text node using the textNode method and appended it to the created div using appendChild. Inserted the new div into the document using insertBefore method.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// PART ONE //
+
+// Examines the document //
+
 // Logs details of  the entire document in the console
 // console.dir(document);
 
@@ -46,7 +50,7 @@
 // console.log(document.getElementById('header-title'));
 
 // Creates a variable headerTitle and assigns the element with the selected id to the variable
-var headerTitle = document.getElementById('header-title');
+// var headerTitle = document.getElementById('header-title');
 
 // Creates a variable header and assigns the element with the selected id to the variable
 // var header = document.getElementById('main-header');
@@ -64,7 +68,7 @@ var headerTitle = document.getElementById('header-title');
 // GETELEMENTSBYCLASSNAME //
 
 // Creates a variaable and assigns all elements with the selected class name
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 
 // Logs the HTML collection associated with the variable 'items'
 // console.log(items);
@@ -120,10 +124,10 @@ for( let i = 0; i < items.length; i++){
 
 // QUERYSELECTOR //
 // Creates a variable 'header' and assigns the selected id 'main-header' to the variable header
-let header = document.querySelector('#main-header');
+// let header = document.querySelector('#main-header');
 
 // Changes the css border bottom style of the variable header
-header.style.borderBottom = 'solid 4px #ccc';
+// header.style.borderBottom = 'solid 4px #ccc';
 
 // Creates a variable 'input' and assigns the selected html element 'input' to the variable input
 // let input = document.querySelector('input');
@@ -135,7 +139,7 @@ header.style.borderBottom = 'solid 4px #ccc';
 let submit = document.querySelector('input[type="submit"]');
 
 // Changes the value of the submit variable to 'SEND'
-submit.value = "SEND";
+submit.value = "Add";
 
 // Creates a variable 'item' and assigns the first child of the selected class '.list-group-item' to the variable 'item'
 // let item = document.querySelector('.list-group-item');
@@ -161,7 +165,7 @@ submit.value = "SEND";
 // QUERYSELECTORALL //
 
 // Creates a variable titles and assigns all HTML elements with the selected class '.title' to the variable 'titles'
-let titles = document.querySelectorAll('.title');
+// let titles = document.querySelectorAll('.title');
 
 // Logs the node list of HTML elements with the '.title' class
 // console.log(titles);
@@ -170,13 +174,131 @@ let titles = document.querySelectorAll('.title');
 // titles[0].textContent = 'Hello';
 
 // Creates a variable 'odd' and assings all the odd li items in the document to the variable
-let odd = document.querySelectorAll('li:nth-child(odd)');
+// let odd = document.querySelectorAll('li:nth-child(odd)');
 
 // Creates a variable 'even' and assings all the even li items in the document to the variable
-let even = document.querySelectorAll('li:nth-child(even)');
+// let even = document.querySelectorAll('li:nth-child(even)');
 
 // Loops through each item and changes the css style for each odd and even item in the li node list
+/*
 for (let i = 0; i < odd.length; i++){
     odd[i].style.backgroundColor = 'orange';
     even[i].style.backgroundColor = 'whitesmoke';
 }
+*/
+
+// PART TWO //
+
+// TRAVERSING THE DOM //
+
+let itemList = document.querySelector('#items');
+
+// Using parentNode
+
+// Logs the parent node of the variable 'itemList' into the console.
+// console.log(itemList.parentNode);
+
+// Changes the background color of the parent node to the itemList to a light grey color.
+// itemList.parentNode.style.backgroundColor = '#f3f3f3';
+
+// Logs the parent node of the parent node of the parent node of the itemList into the console.
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// Using parentElement
+
+// Logs the parent element of the variable 'itemList' into the console.
+// console.log(itemList.parentElement);
+
+// Changes the background color of the parent element to the itemList to a light grey color.
+// itemList.parentElement.style.backgroundColor = '#f3f3f3';
+
+// Logs the parent element of the parent element of the itemList into the console.
+// console.log(itemList.parentElement.parentElement);
+
+// Using childNodes
+
+// Logs a node list of the children in the variable 'itemList' including any whitespaces and line breaks.
+// console.log(itemList.childNodes);
+
+// Using children
+
+// Logs a HTML collection of the children in the variable 'itemList' without any whitespaces or linebreaks
+// console.log(itemList.children);
+
+// Using firstChild
+
+// Logs the first node on the node list
+// console.log(itemList.firstChild);
+
+// Using firstElementChild
+
+// Logs the first child element on the HTML collection
+// console.log(itemList.firstElementChild);
+
+// Changes the css font weight style of the first element child on the HTML collection to bold.
+// itemList.firstElementChild.style.fontWeight = 'bold';
+
+// Using lastChild
+
+// Logs the last node on the node list
+// console.log(itemList.lastChild);
+
+// Using lasttElementChild
+
+// Logs the lasst child element on the HTML collection
+// console.log(itemList.lastElementChild);
+
+// Changes the text content of the last element child on the HTML collection to 'Hello 4'.
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// Using nextSibling
+
+//Logs the next sibling of the itemList variable on the node list even if it is a whitespace.
+// console.log(itemList.nextSibling);
+
+// Using nextElementSibling
+
+// Logs the next sibling element of the itemList variable on the HTML collection.
+// console.log(itemList.nextElementSibling);
+
+// Using previousSibling
+
+//Logs the previous sibling of the itemList variable on the node list even if it is a whitespace.
+// console.log(itemList.previousSibling);
+
+// Using previousElementSibling
+
+// Logs the previous sibling element of the itemList variable on the HTML collection.
+// console.log(itemList.previousElementSibling);
+
+// createElement
+
+// Creates a variable newDiv and assigns a newly created element 'div' to it.
+let newDiv = document.createElement('div');
+
+// Add a class to the div
+newDiv.className = 'hello';
+
+// Add an id to the div
+newDiv.id = 'hello1';
+
+// Adds a title attribute with the value 'Hello Div' to the div.
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create a text node
+let newDivText = document.createTextNode('Hello World');
+
+// Add the text node newDivText to the div.
+newDiv.appendChild(newDivText);
+
+// Creates a variable 'container' and assigns the selected class '.container' in the header tag to it.
+let container = document.querySelector('header .container');
+
+// Creates a variable 'h1' and assigns the selected HTML element h1 in the header tag to it.
+let h1 = document.querySelector('header h1');
+
+// Logs the created div in the console.
+console.log(newDiv);
+
+// Inserts the newDiv variable before the h1 variable inside the container variable in the document.
+container.insertBefore(newDiv, h1);
